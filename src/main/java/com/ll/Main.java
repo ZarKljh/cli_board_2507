a4875842187b3e6a36a55be58521dd5c40f5c28e
ZarKljh <zarkljh@gmail.com>
2025-07-11 17:28:59 +0900

    step 1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,17 +1,25 @@
 package com.ll;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==============   게시판 앱   ===================");
+        System.out.println("==============  종료 : exit  ===================");
+
+        // 게시판앱 시작
+        // while문으로 입력을 무한하게 받는다
+        while(true){
+            System.out.print("명령어: ");
+            String inputStrData = sc.nextLine();
+            //exit 입력시 게시판앱 종료
+            if(inputStrData.equals("exit")){
+                System.out.println("==============게시판 앱을 종료합니다===================");
+                break;
+            }
         }
+        sc.close();
     }
 }
