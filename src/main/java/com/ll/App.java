8e9f02b30b8f800caff9db6155ab7850ae5ee542
ZarKljh <zarkljh@gmail.com>
2025-07-14 14:02:48 +0900

    step 4-2

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,46 @@
+package com.ll;
+import java.util.Scanner;
+
+
+public class App {
+    Scanner sc;
+
+    App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+
+        int lastId = 0;
+
+        System.out.println("==============   게시판 앱   ===================");
+        System.out.println("==============  등록 : signin  ===================");
+        System.out.println("==============  종료 : exit  ===================");
+
+        // 게시판앱 시작
+        // while문으로 입력을 무한하게 받는다
+        while(true){
+            System.out.print("명령어: ");
+            //trim()을 붙이면 명령어 앞뒤에 생기는 공백을 없애준다
+            String command = sc.nextLine().trim();
+            //exit 입력시 게시판앱 종료
+            if(command.equals("exit")){
+                System.out.println("==============게시판 앱을 종료합니다===================");
+                break;
+            } else if(command.equals("signin")) {
+
+                System.out.print("제목을 입력하세요 : ");
+                String inputTitle = sc.nextLine().trim();
+                System.out.println("제목: " + inputTitle);
+
+                System.out.print("내용을 입력하세요 : ");
+                String inputContent = sc.nextLine().trim();
+                System.out.println("내용: " + inputContent);
+
+                //게시판번호++
+                lastId ++;
+                System.out.println(lastId + "번 게시물이 등록되었습니다");
+            }
+        }
+    }
+}
